3bab57345d20ae58149bfc4e5b0b660b4c94d684
Michael <michael@oneai.com>
2021-12-17 06:43:09 +0200

    added missing skill

--- src/oneai/skills.py
+++ src/oneai/skills.py
@@ -2,11 +2,17 @@ from dataclasses import dataclass
 
 from oneai.classes import skillclass
 
+@skillclass(name='enhance', iswriting=True)
+class EnhanceTranscription: pass
+
 @skillclass(name='summarize', iswriting=True, param_fields=['min_length', 'max_length'])
 @dataclass
 class Summarize:
     min_length: int = 5
     max_length: int = 100
+
+@skillclass('emotions')
+class EmotionDetection: pass
 
 @skillclass(name='entities')
 class EntityDetection: pass
@@ -17,5 +23,5 @@ class KeywordDetection: pass
 @skillclass(name='highlights')
 class Highlighting: pass
 
-@skillclass(name='enhance', iswriting=True)
-class EnhanceTranscription: pass
+@skillclass(name='sentences')
+class SplitToSentences: pass
