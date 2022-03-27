40eedc6aa45f516be62821c533aaa1da0efe2b4a
Michael <michael@oneai.com>
2022-03-27 15:06:28 +0300

    moved API requests to a separate file

--- src/oneai/pipeline.py
+++ src/oneai/pipeline.py
@@ -1,16 +1,14 @@
-import aiohttp
 import asyncio
 import oneai
 
 from typing import Awaitable, List, Literal
 from oneai.classes import Skill, LabeledText
+from oneai.requests import send_batch_request, send_single_request
 
-
-MAX_CONCURRENT_REQUESTS = 4
 
 class Pipeline:
     def __init__(self, steps: List[Skill], input_type: Literal['article', 'transcription']=None, api_key: str=None) -> None:
-        self.steps = steps # todo: validate (based on input_type)
+        self.steps = steps  # todo: validate (based on input_type)
         self.input_type = input_type
         self.api_key = api_key
 
@@ -39,14 +37,12 @@ class Pipeline:
         input_type: Literal['article', 'converstion']=None,
         api_key: str=None
     ) -> Awaitable[List[LabeledText]]:
-        timeout = aiohttp.ClientTimeout(total=6000)
-        async with aiohttp.ClientSession(timeout=timeout) as session:
-            return await self._send_request(
-                session,
-                text,
-                api_key=api_key or self.api_key or oneai.api_key,
-                input_type=input_type or self.input_type or 'article'
-            )
+        return await send_single_request(
+            text,
+            self.to_json(),
+            api_key=api_key or self.api_key or oneai.api_key,
+            input_type=input_type or self.input_type or 'article'
+        )
 
     def run_batch(
         self,
@@ -62,43 +58,12 @@ class Pipeline:
         input_type: Literal['article', 'converstion']=None,
         api_key: str=None
     ) -> Awaitable[List[List[LabeledText]]]:
-        size = len(batch)
-        results = [None] * size
-        
-        async def send_requests(session, task_id):
-            for i in range(task_id, size, MAX_CONCURRENT_REQUESTS):
-                results[i] = await self._send_request(
-                    session,
-                    batch[task_id],
-                    api_key=api_key or self.api_key or oneai.api_key,
-                    input_type=input_type or self.input_type or 'article'
-                )
-        
-        timeout = aiohttp.ClientTimeout(total=6000)
-        tasks = []
-        async with aiohttp.ClientSession(timeout=timeout) as session:
-            for i in range(min(MAX_CONCURRENT_REQUESTS, size)):
-                task = asyncio.create_task(send_requests(session, i))
-                tasks.append(task)
-            await asyncio.gather(*tasks)
-            return results
-
-    async def _send_request(self, session, text, input_type, api_key) -> Awaitable[List[LabeledText]]:
-        headers = {
-            'api-key': api_key,
-            'Content-Type': 'application/json'
-        }
-        request = {
-            'text': text,
-            'steps': self.to_json(),
-            'input_type': input_type
-        }
-        async with session.post(oneai.URL, headers=headers, json=request) as resp:
-            if resp.status != 200:
-                raise Exception # todo error types
-            else:
-                response = await resp.json()
-                return [LabeledText(output['text'], output['labels']) for output in response['output']]
+        return await send_batch_request(
+            batch,
+            self.to_json(),
+            api_key=api_key or self.api_key or oneai.api_key,
+            input_type=input_type or self.input_type or 'article'
+        )
 
     def __repr__(self) -> str:
         return f'oneai.Pipeline({self.steps})'
---
+++ src/oneai/requests.py
@@ -0,0 +1,79 @@
+import asyncio
+from typing import Awaitable, Generator, List
+
+import aiohttp
+import oneai
+
+from oneai.classes import LabeledText
+
+MAX_CONCURRENT_REQUESTS = 4
+
+
+async def _send_request(
+    session: aiohttp.ClientSession,
+    text: str,
+    steps: dict,
+    input_type: str,
+    api_key: str
+) -> Awaitable[List[LabeledText]]:
+    headers = {
+        'api-key': api_key,
+        'Content-Type': 'application/json'
+    }
+    request = {
+        'text': text,
+        'steps': steps,
+        'input_type': input_type
+    }
+    async with session.post(oneai.URL, headers=headers, json=request) as resp:
+        if resp.status != 200:
+            raise Exception  # todo error types
+        else:
+            response = await resp.json()
+            return [LabeledText(output['text'], output['labels']) for output in response['output']]
+
+
+async def send_single_request(
+    text: str,
+    steps: dict,
+    input_type: str,
+    api_key: str
+) -> Awaitable[List[LabeledText]]:
+    timeout = aiohttp.ClientTimeout(total=6000)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        return await _send_request(
+            session,
+            text,
+            steps,
+            input_type,
+            api_key
+        )
+
+
+async def send_batch_request(
+    batch: List[str],
+    steps: dict,
+    input_type: str,
+    api_key: str
+) -> Awaitable[List[List[LabeledText]]]:
+    size = len(batch)
+    results = [None] * size
+
+    async def req_worker(session, task_id):
+        for i in range(task_id, size, MAX_CONCURRENT_REQUESTS):
+            results[i] = await _send_request(
+                session,
+                batch[task_id],
+                steps,
+                input_type,
+                api_key
+            )
+
+    timeout = aiohttp.ClientTimeout(total=6000)
+    tasks = []
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        for i in range(min(MAX_CONCURRENT_REQUESTS, size)):
+            task = asyncio.create_task(req_worker(session, i))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+        return results
