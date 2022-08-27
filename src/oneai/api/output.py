127bf62e23bf30167486c9ffb0444f920a40652d
michgur <michael@thegur.net>
2022-08-27 18:04:59 +0300

    moved build_output to a separate file

---
+++ src/oneai/api/output.py
@@ -0,0 +1,74 @@
+from copy import copy
+from typing import List
+
+from oneai.classes import Label, Labels, Output, Skill, TextContent
+from oneai.parsing import parse_conversation
+
+
+def build_output(
+    skills: List[Skill], raw_output: dict,
+) -> Output:
+    def get_text(index) -> TextContent:
+        # get the input text for this Output object. use index=-1 to get the original input text
+        # text can be returned as a simple str or parsed to match a given input type
+        text = (
+            raw_output["output"][index]["text"]
+            if index >= 0
+            else raw_output["input_text"]
+        )
+        try:
+            return parse_conversation(text)
+        except:
+            return text
+
+    def split_pipeline(skills: List[Skill], i: int):
+        # split pipeline at a generator Skill
+        first, second = skills[: i + 1], skills[i + 1 :]
+        if hasattr(skills[i], "output_attr1") and skills[i].output_attr1:
+            # handle skills that create both text and labels
+            clone = copy(skills[i])
+            clone.is_generator = False
+            clone.output_attr = skills[i].output_attr1
+            second = (clone, *second)
+        return first, second
+
+    def build_internal(
+        output_index: int, skills: List[Skill], input_type: type
+    ) -> "Output":
+        text = get_text(output_index)
+        # temporary fix- if 1st skill is not a generator, use input_text, not output[0].text,
+        # since output[0].text is corrupted (not parsable) for conversation inputs
+        output_index = max(output_index, 0)
+        labels = [
+            Label.from_dict(label)
+            for label in raw_output["output"][output_index]["labels"]
+        ]
+        data = []
+        for i, skill in enumerate(skills):
+            if skill.is_generator:
+                skills, next_skills = split_pipeline(skills, i)
+                data.append(
+                    build_internal(
+                        output_index + 1, next_skills, skill.output_type or input_type
+                    )
+                )
+                break
+            else:
+                data.append(
+                    Labels(filter(lambda label: label.type == skill.label_type, labels))
+                )
+        return Output(text=text, skills=list(skills), data=data)
+
+    generator = raw_output["output"][0].get("text_generated_by_step_id", 0) - 1
+    if generator < 0:
+        return build_internal(-1, skills)
+    else:
+        # edge case- first Skill is a generator, or a generator preceded by Skills that didn't generate output
+        # in this case the API will skip these Skills,
+        # so we need to create filler objects to match the expected structure
+        skills, next_skills = split_pipeline(skills, generator)
+        return Output(
+            text=get_text(-1),
+            skills=list(skills),
+            data=[Labels()] * generator + [build_internal(0, next_skills)],
+        )
--- src/oneai/api/pipeline.py
+++ src/oneai/api/pipeline.py
@@ -1,11 +1,11 @@
-from copy import copy
 import json
-from typing import Awaitable, List
-import aiohttp
 import urllib.parse
-import oneai, oneai.api
+from typing import Awaitable, List
 
-from oneai.classes import Input, Label, Labels, Output, Skill
+import aiohttp
+import oneai, oneai.api
+from oneai.api.output import build_output
+from oneai.classes import Input, Output, Skill
 from oneai.exceptions import handle_unsuccessful_response, validate_api_key
 
 endpoint_default = "api/v0/pipeline"
@@ -73,70 +73,3 @@ async def post_pipeline_async_file(
             await handle_unsuccessful_response(response)
         else:
             return await response.json()
-
-
-def build_output(
-    skills: List[Skill], raw_output: dict, input_type: type = str
-) -> Output:
-    def get_text(index, input_type):
-        # get the input text for this Output object. use index=-1 to get the original input text
-        # text can be returned as a simple str or parsed to match a given input type
-        text = (
-            raw_output["output"][index]["text"]
-            if index >= 0
-            else raw_output["input_text"]
-        )
-        return input_type.parse(text)
-
-    def split_pipeline(skills: List[Skill], i: int):
-        # split pipeline at a generator Skill
-        first, second = skills[: i + 1], skills[i + 1 :]
-        if hasattr(skills[i], "output_attr1") and skills[i].output_attr1:
-            # handle skills that create both text and labels
-            clone = copy(skills[i])
-            clone.is_generator = False
-            clone.output_attr = skills[i].output_attr1
-            second = (clone, *second)
-        return first, second
-
-    def build_internal(
-        output_index: int, skills: List[Skill], input_type: type
-    ) -> "Output":
-        text = get_text(output_index, input_type).raw
-        # temporary fix- if 1st skill is not a generator, use input_text, not output[0].text,
-        # since output[0].text is corrupted (not parsable) for conversation inputs
-        output_index = max(output_index, 0)
-        labels = [
-            Label.from_dict(label)
-            for label in raw_output["output"][output_index]["labels"]
-        ]
-        data = []
-        for i, skill in enumerate(skills):
-            if skill.is_generator:
-                skills, next_skills = split_pipeline(skills, i)
-                data.append(
-                    build_internal(
-                        output_index + 1, next_skills, skill.output_type or input_type
-                    )
-                )
-                break
-            else:
-                data.append(
-                    Labels(filter(lambda label: label.type == skill.label_type, labels))
-                )
-        return Output(text=text, skills=list(skills), data=data)
-
-    generator = raw_output["output"][0].get("text_generated_by_step_id", 0) - 1
-    if generator < 0:
-        return build_internal(-1, skills, input_type)
-    else:
-        # edge case- first Skill is a generator, or a generator preceded by Skills that didn't generate output
-        # in this case the API will skip these Skills,
-        # so we need to create filler objects to match the expected structure
-        next_input_type = skills[generator].output_type or input_type
-        skills, next_skills = split_pipeline(skills, generator)
-        return Output(
-            text=get_text(-1, input_type).raw,
-            skills=list(skills),
-            data=[Labels()] * generator + [build_internal(0, next_skills, next_input_type)],
-        )
