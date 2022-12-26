f8b7ce24f2d0e83f07a9075f3e526b8e06acce65
michgur <michael@thegur.net>
2022-12-26 12:10:52 +0200

    removed print

--- src/oneai/api/pipeline.py
+++ src/oneai/api/pipeline.py
@@ -49,7 +49,6 @@ async def post_pipeline(
     validate_api_key(api_key)
 
     request = build_request(input, steps, multilingual, True)
-    print(request)
     url = f"{oneai.URL}/{endpoint_default}"
     headers = {
         "api-key": api_key,
