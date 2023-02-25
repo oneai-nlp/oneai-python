fedfd653ab780103bbe39bb9ca7efdd2b632a863
michgur <michael@thegur.net>
2023-02-25 20:38:57 +0200

    refactor

--- src/oneai/classes.py
+++ src/oneai/classes.py
@@ -20,6 +20,7 @@ from typing import (
     Union,
     Hashable,
 )
+from typing_extensions import dataclass_transform
 from warnings import warn
 
 
@@ -107,6 +108,75 @@ class Skill:
             "skill": self.api_name,
             "params": self.params,
         }
+
+
+@dataclass_transform()
+def skillclass(
+    api_name: str = "",
+    text_attr: Optional[str] = None,
+    labels_attr: Optional[str] = None,
+):
+    """
+    A decorator for defining a Language Skill class. Decorate subclasses of `Skill` with this to provide default values for instance attributes.
+
+    ## Example
+
+    >>> @skillclass(api_name="my-skill", text_attr="my_result")
+    ... class MySkill(Skill):
+    ...     ratio: float = 0.2
+    >>> s = Summarize(ratio=0.5)
+    >>> s.ratio
+    0.5
+    >>> pipeline = Pipeline([s])
+    >>> output = pipeline.run("Text to be processed with MySkill")
+    >>> output.my_result
+    "Result text, processed with MySkill"
+    """
+
+    def wrap(cls):
+        if not issubclass(cls, Skill):
+            warn(
+                f"warning: class {cls.__name__} decorated with @skillclass does not inherit Skill",
+                stacklevel=2,
+            )
+
+        # remove class variables
+        classVars = {
+            k: getattr(cls, k, None)
+            for k in cls.__annotations__
+            if k not in Skill.__annotations__
+        }
+        for k in classVars:
+            if hasattr(cls, k):
+                delattr(cls, k)
+
+        def __init__(self, **params):
+            Skill.__init__(
+                self,
+                api_name=api_name,
+                text_attr=text_attr,
+                labels_attr=labels_attr,
+                params=params,
+            )
+
+            for k, v in classVars.items():
+                if k not in params:
+                    setattr(self, k, v)
+
+        def __getattr__(self, name):
+            return self.params[name]
+
+        def __setattr__(self, name, value):
+            if "params" not in self.__dict__:
+                return object.__setattr__(self, name, value)
+            self.params[name] = value
+
+        cls.__init__ = __init__
+        cls.__getattr__ = __getattr__
+        cls.__setattr__ = __setattr__
+        return cls
+
+    return wrap
 
 
 class Input(Generic[TextContent]):
